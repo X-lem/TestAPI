4cc21fab66c6142ba03520691b2ac4d29d7967a2
Regis <X-lem@hotmail.com>
2017-11-23 10:00:27 -0700

    Create test API

---
+++ server/routes/index.js
@@ -0,0 +1,15 @@
+const todosController = require('../controllers').todos;
+
+module.exports = (app) => {
+  app.get('/api', (req, res) => res.status(200).send({
+    message: 'Welcome to the Todos API!',
+  }));
+
+  app.get('/api/todos', todosController.list);
+  // app.post('/api/todos/:todoId/items', todoItemsController.create);
+  app.post('/api/todos', todosController.create);
+  app.get('/api/todos/:todoId', todosController.retrieve);
+  app.put('/api/todos/:todoId', todosController.update);
+  app.delete('/api/todos/:todoId', todosController.destroy);
+
+};
