e855bd9ff70673fc05d27890d1815da749552887
Regis <X-lem@hotmail.com>
2017-11-24 14:50:08 -0700

    Minor text fixes

--- server/controllers/todoitems.js
+++ server/controllers/todoitems.js
@@ -12,29 +12,29 @@ module.exports = {
   },
 
   update(req, res) {
-    return TodoItem
-      .find({
-          where: {
-            id: req.params.todoItemId,
-            todoId: req.params.todoId,
-          },
-        })
-      .then(todoItem => {
-        if (!todoItem) {
-          return res.status(404).send({
-            message: 'TodoItem Not Found',
-          });
-        }
-
-        return todoItem
-          .update({
-            content: req.body.content || todoItem.content,
-            complete: req.body.complete || todoItem.complete,
-          })
-          .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
-          .catch(error => res.status(400).send(error));
+  return TodoItem
+    .find({
+        where: {
+          id: req.params.todoItemId,
+          todoId: req.params.todoId,
+        },
       })
-      .catch(error => res.status(400).send(error));
+    .then(todoItem => {
+      if (!todoItem) {
+        return res.status(404).send({
+          message: 'TodoItem Not Found',
+        });
+      }
+
+      return todoItem
+        .update({
+          content: req.body.content || todoItem.content,
+          complete: req.body.complete || todoItem.complete,
+        })
+        .then(updatedTodoItem => res.status(200).send(updatedTodoItem))
+        .catch(error => res.status(400).send(error));
+    })
+    .catch(error => res.status(400).send(error));
   },
 
   destroy(req, res) {
@@ -58,5 +58,5 @@ module.exports = {
           .catch(error => res.status(400).send(error));
       })
       .catch(error => res.status(400).send(error));
-  },
+  }
 };
